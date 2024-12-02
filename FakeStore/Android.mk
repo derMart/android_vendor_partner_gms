7aec2cabd1471aece3ef9fc2e07eed0000891436
derMart <derMart@users.noreply.github.com>
2024-12-02 19:03:50 +0100

    replace gmscore with own build version
    Change-Id: I6d11d5ffcd67ab8f55e9ac083f3df5dde1a449ce

--- FakeStore/Android.mk
+++ FakeStore/Android.mk
@@ -22,6 +22,10 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml default-permissions-com.android.vending.xml
+# these lines may break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
